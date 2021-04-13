37c8ba6b90642da2bece1e6ce13e4efc28b5d415
Rahul Rajan <rahul.rajanus@gmail.com>
2021-04-13 18:08:13 -0400

    sync

--- dataset.py
+++ dataset.py
@@ -6,6 +6,7 @@ import os.path
 import numpy as np
 from numpy.random import randint
 
+
 class VideoRecord(object):
     def __init__(self, row):
         self._data = row
@@ -40,21 +41,25 @@ class TSNDataSet(data.Dataset):
         self.test_mode = test_mode
 
         if self.modality == 'RGBDiff':
-            self.new_length += 1# Diff needs one more image to calculate diff
+            self.new_length += 1  # Diff needs one more image to calculate diff
 
         self._parse_list()
 
     def _load_image(self, directory, idx):
+        print("LOG", directory, idx)
         if self.modality == 'RGB' or self.modality == 'RGBDiff':
             return [Image.open(os.path.join(directory, self.image_tmpl.format(idx))).convert('RGB')]
         elif self.modality == 'Flow':
-            x_img = Image.open(os.path.join(directory, self.image_tmpl.format('x', idx))).convert('L')
-            y_img = Image.open(os.path.join(directory, self.image_tmpl.format('y', idx))).convert('L')
+            x_img = Image.open(os.path.join(
+                directory, self.image_tmpl.format('x', idx))).convert('L')
+            y_img = Image.open(os.path.join(
+                directory, self.image_tmpl.format('y', idx))).convert('L')
 
             return [x_img, y_img]
 
     def _parse_list(self):
-        self.video_list = [VideoRecord(x.strip().split(' ')) for x in open(self.list_file)]
+        self.video_list = [VideoRecord(x.strip().split(' '))
+                           for x in open(self.list_file)]
 
     def _sample_indices(self, record):
         """
@@ -63,28 +68,35 @@ class TSNDataSet(data.Dataset):
         :return: list
         """
 
-        average_duration = (record.num_frames - self.new_length + 1) // self.num_segments
+        average_duration = (record.num_frames -
+                            self.new_length + 1) // self.num_segments
         if average_duration > 0:
-            offsets = np.multiply(list(range(self.num_segments)), average_duration) + randint(average_duration, size=self.num_segments)
+            offsets = np.multiply(list(range(self.num_segments)), average_duration) + \
+                randint(average_duration, size=self.num_segments)
         elif record.num_frames > self.num_segments:
-            offsets = np.sort(randint(record.num_frames - self.new_length + 1, size=self.num_segments))
+            offsets = np.sort(randint(record.num_frames -
+                                      self.new_length + 1, size=self.num_segments))
         else:
             offsets = np.zeros((self.num_segments,))
         return offsets + 1
 
     def _get_val_indices(self, record):
         if record.num_frames > self.num_segments + self.new_length - 1:
-            tick = (record.num_frames - self.new_length + 1) / float(self.num_segments)
-            offsets = np.array([int(tick / 2.0 + tick * x) for x in range(self.num_segments)])
+            tick = (record.num_frames - self.new_length + 1) / \
+                float(self.num_segments)
+            offsets = np.array([int(tick / 2.0 + tick * x)
+                                for x in range(self.num_segments)])
         else:
             offsets = np.zeros((self.num_segments,))
         return offsets + 1
 
     def _get_test_indices(self, record):
 
-        tick = (record.num_frames - self.new_length + 1) / float(self.num_segments)
+        tick = (record.num_frames - self.new_length + 1) / \
+            float(self.num_segments)
 
-        offsets = np.array([int(tick / 2.0 + tick * x) for x in range(self.num_segments)])
+        offsets = np.array([int(tick / 2.0 + tick * x)
+                            for x in range(self.num_segments)])
 
         return offsets + 1
 
@@ -92,7 +104,8 @@ class TSNDataSet(data.Dataset):
         record = self.video_list[index]
 
         if not self.test_mode:
-            segment_indices = self._sample_indices(record) if self.random_shift else self._get_val_indices(record)
+            segment_indices = self._sample_indices(
+                record) if self.random_shift else self._get_val_indices(record)
         else:
             segment_indices = self._get_test_indices(record)
 
